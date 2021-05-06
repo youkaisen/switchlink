c2f3feb2c155927da4c050f3a50edd104a249c57
David Marchand <david.marchand@redhat.com>
2021-05-06 17:25:45 +0200

    ci: Install python elftools for DPDK 21.02.
    pyelftools is now required to build DPDK [1].
    
    1: https://git.dpdk.org/dpdk/commit/?id=f0f93a7adfee
    
    Signed-off-by: David Marchand <david.marchand@redhat.com>
    Signed-off-by: Ian Stokes <ian.stokes@intel.com>

--- .ci/linux-prepare.sh
+++ .ci/linux-prepare.sh
@@ -21,7 +21,7 @@ make -j4 HAVE_LLVM= HAVE_SQLITE= install
 cd ..
 
 pip3 install --disable-pip-version-check --user \
-    flake8 hacking sphinx pyOpenSSL wheel setuptools
+    flake8 hacking sphinx pyOpenSSL wheel setuptools pyelftools
 pip3 install --user --upgrade docutils
 pip3 install --user  'meson==0.47.1'
 
