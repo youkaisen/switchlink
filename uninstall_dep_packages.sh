6d995d451ba9886418934084f481113b9e74d99f
nupuruttarwar <nupur.uttarwar@intel.com>
2022-03-14 21:56:54 -0700

    P4-OVS: Update documentation and fix uninstall script (#43)
    * P4-OVS: Update documentation and fix uninstall script
    
    This patch updates the documentation for install steps of OVS.
    And updates uninstall script for dependent package to ensure both
    old and new version of grpc and protobuf are uninstallable with
    the script
    
    Title: Update documentation and fix uninstall script
    Change-type: DefectResolution
    Signed-off-by: nupuruttarwar <nupur.uttarwar@intel.com>

--- uninstall_dep_packages.sh
+++ uninstall_dep_packages.sh
@@ -31,6 +31,7 @@ FROM THE CUSTOMIZED INSTALL PATH ONLY \
 #First argument is taken as the directory path \
 #for the source code and installables scratch area.
 SRC_DIR=$1/P4OVS_DEPS_SRC_CODE
+WS_DIR=$PWD
 
 if [ -z "$2" ];
 then
@@ -52,19 +53,6 @@ get_num_cores
 echo ""
 echo "Number of Parallel threads used: $NUM_THREADS ..."
 echo ""
-
-
-# Dependencies of netlink library
-if [[ $OS =~ "Fedora" ]]; then
-    sudo dnf remove -y pkgconfig
-    sudo dnf remove -y libnl3-devel
-elif [[ $OS =~ "Ubuntu" ]]; then
-    sudo apt-get remove -y pkg-config
-    sudo apt-get remove -y libnl-route-3-dev
-else
-    sudo yum remove -y pkgconfig
-    sudo yum remove -y libnl3-devel
-fi
 
 #gflags uninstall
 MODULE="gflags"
@@ -98,15 +86,61 @@ sudo ldconfig
 MODULE="protobuf"
 echo "####  Uninstalling the '$MODULE' module ####"
 cd ${SRC_DIR}/$MODULE
-sudo make uninstall
+#If install_manifest file exists, previous installation was using cmake
+if [ -d "build" ]
+then
+  FILE=./build/install_manifest.txt
+  if [ -f $FILE ]
+  then
+    cd build && cat install_manifest.txt | xargs rm -rf
+  fi
+else
+  sudo make uninstall
+fi
 sudo ldconfig
 
 #grpc uninstall
 MODULE="grpc"
 echo "####  Uninstalling the '$MODULE' module ####"
-mkdir -p $SRC_DIR/$MODULE/build
-cd ${SRC_DIR}/$MODULE/build
-cat install_manifest.txt | xargs rm -rf
+#mkdir -p $SRC_DIR/$MODULE/build
+cd ${SRC_DIR}/$MODULE
+if [ -d "build" ]
+then
+  FILE=./build/install_manifest.txt
+  if [ -f $FILE ]
+  then
+    cd build && cat install_manifest.txt | xargs rm -rf
+  fi
+else
+#If grpc v1.17.2 is installed on the system, since 'make uninstall'
+#is not supported. This is the work-around to get install file list
+echo "Rebuilding grpc with cmake to uninstall grpc cleanly since make uninstall is unsupported"
+  #GRPC cmake build has a dependency on golang
+  if [[ $OS =~ "Fedora" ]]; then
+      sudo dnf install -y golang
+  elif [[ $OS =~ "Ubuntu" ]]; then
+      sudo apt-get install -y golang
+  else
+      sudo yum install -y golang
+  fi
+  cd ${SRC_DIR}/$MODULE/third_party/boringssl
+  #PATCH-01-GRPC-BORINGSSL is required to build crypto module of boringssl
+  git apply $WS_DIR/external/PATCH-01-GRPC-BORINGSSL
+  cd ${SRC_DIR}/$MODULE
+  mkdir build
+  cd build
+  cmake -DgRPC_INSTALL=ON -DBUILD_TESTING=OFF $CMAKE_PREFIX ..
+  make $NUM_THREADS
+  sudo make $NUM_THREADS install
+  cat install_manifest.txt | xargs rm -rf
+  if [[ $OS =~ "Fedora" ]]; then
+    sudo dnf remove -y golang
+  elif [[ $OS =~ "Ubuntu" ]]; then
+    sudo apt-get remove -y golang
+  else
+    sudo yum remove -y golang
+  fi
+fi
 sudo ldconfig
 
 #nlohmann uninstall
@@ -118,5 +152,19 @@ sudo ldconfig
 
 echo "Removing SOURCE and INSTALL scratch directories, $SRC_DIR and $INSTALL_DIR"
 rm -rf $SRC_DIR
-if [ "$2"]; then
+if [ "$2" ]
+then
     rm -rf $INSTALL_DIR
+fi
+
+#Uninstall dependencies of netlink library
+if [[ $OS =~ "Fedora" ]]; then
+    sudo dnf remove -y pkgconfig
+    sudo dnf remove -y libnl3-devel
+elif [[ $OS =~ "Ubuntu" ]]; then
+    sudo apt-get remove -y pkg-config
+    sudo apt-get remove -y libnl-route-3-dev
+else
+    sudo yum remove -y pkgconfig
+    sudo yum remove -y libnl3-devel
+fi
