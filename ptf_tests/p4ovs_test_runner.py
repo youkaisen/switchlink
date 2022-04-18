8281332ad578e8e809700d6cda33632d0005b1d8
dkaranjai <dwaipayan.karanjai@intel.com>
2022-04-18 13:55:04 +0530

    Task14400 script to run all automated tests (#75)
    * initial commit
    
    * os getcwd to source pre test
    
    * getting sde install, p4ovs and dep path from cli args
    
    * minor edit in tests_to_run.txt
    
    * added support for verbose mode, logging
    
    * bug fix - verbose and logging
    
    * updating Makefile.am
    
    * added support for hashing out tests from suite file
    
    * printing output log in console
    
    * Updated README with instruction run multiple tests in a suite
    
    * Support for printing consolidated output in log
    
    * consolidated output
    
    * consolidated output 2
    
    * Consolidated output continued from pull 75

---
+++ ptf_tests/p4ovs_test_runner.py
@@ -0,0 +1,124 @@
+import subprocess
+import argparse
+import os
+import sys
+import datetime
+import re
+
+from itertools import dropwhile
+
+class MyParser(argparse.ArgumentParser):
+    def print_help(self):
+        print ("""
+usage: p4ovs_test_runner.py [-h] -f FILE -s P4SDE_INSTALL_PATH -o P4OVS_INSTALL_PATH [-d P4DEP_INSTALL_PATH] [-l LOG_FILE] [--verbose]
+
+mandatory arguments:
+    -f FILE, --file FILE  Reads the test suite file default location ptf_tests/ . if kept in a different location, then mention absolute file name. This
+                        file consists tests scripts to run (without .py extension) and the corresponding "test-params"
+    -s P4SDE_INSTALL_PATH, --p4sde_install_path P4SDE_INSTALL_PATH
+                        Absolute P4SDE Install path
+    -o P4OVS_INSTALL_PATH, --p4ovs_install_path P4OVS_INSTALL_PATH
+                        Absolute P4OVS Install path
+
+optional arguments:
+    -h, --help            show this help message and exit
+    -d P4DEP_INSTALL_PATH, --p4dep_install_path P4DEP_INSTALL_PATH
+                        Absolute P4OVS Dependency Install path
+    -l LOG_FILE, --log_file LOG_FILE
+                        name of the log file, by default located in ptf_tests/
+    --verbose prints ptf logs in the console
+    """)
+
+help_message_f = """Reads the test suite file
+default location ptf_tests/ .
+if kept in a different location, then mention absolute file name.
+This file consists tests scripts to run (without .py extension)
+and the corresponding "test-params"
+"""
+
+parser = MyParser()
+parser.add_argument('-f', '--file', type=str, required=True, help=help_message_f)
+parser.add_argument('-s', '--p4sde_install_path', type=str, required=True,
+                    help="Absolute P4SDE Install path")
+parser.add_argument('-o', '--p4ovs_install_path', type=str, required=True,
+                    help="Absolute P4OVS Install path")
+parser.add_argument('-d', '--p4dep_install_path', type=str, required=False,
+                    help="Absolute P4OVS Dependency Install path")
+parser.add_argument('-l', '--log_file', type=str, required=False,
+                    help="name of the log file, by default located in ptf_tests/")
+parser.add_argument('--verbose', action="store_true", help="prints ptf logs in the console")
+args = parser.parse_args()
+
+if not args.p4dep_install_path:
+    args.p4dep_install_path = ""
+
+# Check if 'file' exists
+if not os.path.exists(args.file):
+    print(f"File {args.file} doesn't exist")
+    sys.exit()
+
+# Check if ptf is installed as a binary
+out = subprocess.run("ptf --help", shell=True, capture_output=True)
+if not out.stdout:
+    print("ptf is not installed as an executable")
+    sys.exit()
+
+test_to_run = {}
+sequence = []
+with open(args.file) as fh:
+    for i in fh.readlines():
+        if i.startswith("#"):
+            continue
+        items = i.strip().split(":")
+        test_to_run[items[0].strip()] = items[1].strip()
+        sequence.append(items[0])
+test_to_run['sequence'] = sequence
+
+results = {}
+for test in test_to_run['sequence']:
+    process = subprocess.Popen('/bin/bash', stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    cmd = f"""source pre_test.sh {args.p4sde_install_path} {args.p4ovs_install_path} {args.p4dep_install_path}
+    ptf --test-dir tests/ {test} --pypath $PWD --test-params="{test_to_run[test]}" --platform=dummy
+    """
+    print(f"\nRunning {test}\n")
+    out, err = process.communicate(cmd.encode('utf-8'))
+    out = out.decode('utf-8')
+    print(out)
+    # discarding pre_test.sh logs
+    results[test] = '\n'.join([x for x in list(dropwhile(lambda x: "Using packet manipulation module" not in x,
+                                                         out.split('\n'))) if x])
+print(results)
+summary = []
+if args.log_file:
+    fh = open(args.log_file, "w")
+    fh.truncate()
+    fh.seek(0)
+    e = datetime.datetime.now()
+    fh.write(f"P4OVS tests log as on : {e.strftime('%Y-%m-%d %H:%M:%S')}")
+    for test in test_to_run['sequence']:
+        if args.verbose:
+            fh.write("\n\n"+"="*20+f"\n{test}\n"+"="*20+"\n")
+            fh.write(results[test])
+
+pattern = r"Test has (PASSED|FAILED)"
+for test in test_to_run['sequence']:
+    for line in results[test].split('\n'):
+        m = re.match(pattern, line)
+        if m:
+            summary.append((test, m.groups()[0]))
+
+summary_out = "\n\n"+"="*20+"\nSummary\n"+"="*20+"\n"
+for i in summary:
+    summary_out += f"{i[0]} : {i[1]}\n"
+
+passed_count = len([True for i in summary if i[1] == "PASSED"])
+failed_count = len([True for i in summary if i[1] == "FAILED"])
+
+cons_out = f"Ran {len(summary)} test(s), {passed_count} Passed, {failed_count} Failed."
+if args.log_file:
+    fh.write(summary_out)
+    fh.write(f"\n\n{cons_out}")
+    fh.close()
+
+print(summary_out)
+print(f"\n\n{cons_out}")
