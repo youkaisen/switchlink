cebb14d27177817fb3f904744a1a1e4cb138fbf9
Koushik kopanati <koushik.kopanati@intel.com>
2022-06-17 13:47:26 -0700

    added license header

--- uninstall_dep_packages.sh
+++ uninstall_dep_packages.sh
@@ -64,8 +64,15 @@ sudo ldconfig
 #gtest uninstall
 MODULE="googletest"
 echo "####  Uninstalling the '$MODULE' module ####"
-cd $SRC_DIR/$MODULE/build
-sudo make uninstall
+cd $SRC_DIR/
+sudo rm -rf $MODULE
+sudo ldconfig
+
+#gmock-global uninstall
+MODULE="gmock-global"
+echo "####  Removing the '$MODULE' module ####"
+cd $SRC_DIR
+sudo rm -rf $MODULE 
 sudo ldconfig
 
 #glog uninstall
