55f24a0c3a3e80a508443f209ba850de8226458d
vrahikax <102287290+vrahikax@users.noreply.github.com>
2022-06-28 12:28:24 +0530

    Update local_connection.py
    Remove exit status method as not needed.

--- ptf_tests/common/lib/local_connection.py
+++ ptf_tests/common/lib/local_connection.py
@@ -22,23 +22,6 @@ class Local(object):
             err = err.decode('utf-8')
         return out.decode('utf-8'), self.process.returncode, err
 
-    def execute_command_get_exit_status(self, command):
-        """To execute localhost commands and return exit status only
-
-        :param command: command to execute locally
-        :type command: string e.g. "ls -lh"
-        :return: pass or fail exit status of command
-        :rtype:
-        """
-        self.process = subprocess.Popen('/bin/bash', stdin=subprocess.PIPE,
-                                        stdout=subprocess.PIPE)
-        out, err = self.process.communicate(command.encode('utf-8'))
-        if err:
-            err = err.decode('utf-8')
-            return -1
-        else:
-            return 0
-
     def tear_down(self):
         """
         Yet to implement
