cfadf034a284987380819570e2818b84a517538f
Sandeep N <sandeep.nagapattinam@intel.com>
2022-09-15 16:23:28 +0000

    Changes to reduce build time and size
    - Because of debug option, size of image is increased.
    Removing build with -g option. User need to add this flag if needed for debugging.
    - Disabling SSL when we build with default path as well.
    
    Signed-off-by: Sandeep N <sandeep.nagapattinam@intel.com>

--- build-p4ovs.sh
+++ build-p4ovs.sh
@@ -41,9 +41,9 @@ fi
 ./boot.sh || exit 1
 if [ ! -z "$DEPS_INSTALL_PATH" ]
 then
-    ./configure --prefix=$DEPS_INSTALL_PATH --with-p4tdi=$SDE_INSTALL_PATH CFLAGS='-O0 -g' --disable-ssl --with-sai || exit 1
+    ./configure --prefix=$DEPS_INSTALL_PATH --with-p4tdi=$SDE_INSTALL_PATH CFLAGS='-O0' --disable-ssl --with-sai || exit 1
 else
-    ./configure --with-p4tdi=$SDE_INSTALL_PATH CFLAGS='-O0 -g' --with-sai || exit 1
+    ./configure --with-p4tdi=$SDE_INSTALL_PATH CFLAGS='-O0' --disable-ssl --with-sai || exit 1
 fi
 
 #Read the number of CPUs in a system and derive the NUM threads
